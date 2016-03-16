2f8fef9b89fc5e26e15b2b3968903fd2d2fa4e0d
Roy Slagle <rslagle@taivara.com>
2016-03-16 15:42:30 -0400

    hardcode path

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 rails_env = deploy[:rails_env]
 path = deploy[:current_path]
 
-default[:cwlogs][:rails][:logfile] = path + '/log/' + rails_env + '.log'
+default[:cwlogs][:rails][:logfile] = 'srv/www/tf_api_int/shared/log/' + rails_env + '.log'
