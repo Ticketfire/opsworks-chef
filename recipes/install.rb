621d8894d7b40ff1d41cb673d8f16d34b987331a
Roy Slagle <rslagle@taivara.com>
2016-03-17 11:28:40 -0400

    cleanup

--- recipes/install.rb
+++ recipes/install.rb
@@ -12,23 +12,25 @@ node[:deploy].each do |application, deploy|
   application_name = node[:deploy][application][:application].gsub(' ', '_')
   rails_env = node[:deploy][application][:rails_env]
   deploy_to = node[:deploy][application][:deploy_to]
+  group_name = node[:opsworks][:stack][:name].gsub(' ', '_') + '_rails_' + rails_env
+  log_file = "#{deploy_to}/shared/log/#{rails_env}.log"
 
   Chef::Log.info("Installing logs agent for #{application_name} in #{rails_env}...")
 
-  template "/tmp/cwlogs_#{application_name}.cfg" do
+  template "/tmp/cwlogs_#{group_name}.cfg" do
     cookbook "opsworks-chef"
     source "cwlogs.cfg.erb"
     owner "root"
     group "root"
     mode 0644
     variables({
-      deploy_to: deploy_to,
-      rails_env: rails_env
+      log_file: log_file,
+      group_name: group_name
     })
   end
 
-  execute "Install CloudWatch Logs agent for rails application #{application_name}" do
-    command "/opt/aws/cloudwatch/awslogs-agent-setup.py -n -r us-east-1 -c /tmp/cwlogs_#{application_name}.cfg"
+  execute "Install CloudWatch Logs agent for rails application #{group_name}" do
+    command "/opt/aws/cloudwatch/awslogs-agent-setup.py -n -r us-east-1 -c /tmp/cwlogs_#{group_name}.cfg"
     not_if { system "pgrep -f aws-logs-agent-setup" }
   end
 
