c839bbc3c63338155ab3907f540a6d8f8fa1844b
Roy Slagle <rslagle@taivara.com>
2016-04-07 15:47:22 -0400

    hard code the secret key base

--- rails_assets_precompile/recipes/default.rb
+++ rails_assets_precompile/recipes/default.rb
@@ -3,7 +3,6 @@ node[:deploy].each do |application, deploy|
   rails_env = node[:deploy][application][:rails_env]
   deploy_to = node[:deploy][application][:deploy_to]
 
-  secret_key_base = env["SECRET_KEY_BASE"]
   Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
 
   # see: https://docs.chef.io/resource_execute.html
@@ -12,7 +11,7 @@ node[:deploy].each do |application, deploy|
     command "bundle exec rake assets:precompile"
     environment(
       "RAILS_ENV" => rails_env,
-      "SECRET_KEY_BASE" => secret_key_base
+      "SECRET_KEY_BASE" => 'c8777843099d21c53123c20624f92918844bd2ffbfe6eb52badabfe0792808089e1280d4fcc67fbb4e8e0be6a632ae087622c2745fb21e502d9b147f33a97da0'
     )
     user 'deploy'
     group 'www-data'
